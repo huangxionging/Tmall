0c9e73d13c3a75f85a82423359538951f5e8dd64
huangxiong <huangxiong@huangxiong.local>
2014-07-29 11:36:14 +0800

    Initial Commit

---
+++ Tmall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tmall
+//
+//  Created by huangxiong on 14-7-29.
+//  Copyright (c) 2014年 New-Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
